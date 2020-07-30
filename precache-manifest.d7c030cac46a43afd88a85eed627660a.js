20ba9db5a9996f8d2d9778fdc38a66d8feec47d7
EdonJung <2donny@naver.com>
2020-07-31 04:13:04 +0900

    Updates

---
+++ precache-manifest.d7c030cac46a43afd88a85eed627660a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effb5b96bd560858b5bbc300620b5419",
+    "url": "/My_Robot/index.html"
+  },
+  {
+    "revision": "ecfca4eb84f6778c9b24",
+    "url": "/My_Robot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "58894ff1fbeac214f5fe",
+    "url": "/My_Robot/static/css/main.00b7a1cb.chunk.css"
+  },
+  {
+    "revision": "ecfca4eb84f6778c9b24",
+    "url": "/My_Robot/static/js/2.e25a8cc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_Robot/static/js/2.e25a8cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58894ff1fbeac214f5fe",
+    "url": "/My_Robot/static/js/main.a71bba1c.chunk.js"
+  },
+  {
+    "revision": "be71456d31ca0e345547",
+    "url": "/My_Robot/static/js/runtime-main.f0279205.js"
+  }
+]);
